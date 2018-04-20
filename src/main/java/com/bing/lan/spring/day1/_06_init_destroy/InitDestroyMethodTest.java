3c6c434ced83d586042b05768f3b03fbc15a9351
lanbing <lanbing@baogongyoucai.com>
2018-04-20 15:52:12 +0800

    init destroy method

---
+++ src/main/java/com/bing/lan/spring/day1/_06_init_destroy/InitDestroyMethodTest.java
@@ -0,0 +1,27 @@
+package com.bing.lan.spring.day1._06_init_destroy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by 蓝兵 on 2018/4/20.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class InitDestroyMethodTest {
+
+    private SomeBean someBean;
+
+    @Autowired
+    private ApplicationContext mBeanFactory;
+
+    @Test
+    public void test() {
+        someBean = mBeanFactory.getBean("someBean", SomeBean.class);
+    }
+}
---
+++ src/main/java/com/bing/lan/spring/day1/_06_init_destroy/SomeBean.java
@@ -0,0 +1,32 @@
+package com.bing.lan.spring.day1._06_init_destroy;
+
+/**
+ * Created by 蓝兵 on 2018/4/19.
+ */
+
+public class SomeBean {
+
+    public static SomeBean create() {
+        System.out.println("create(): 工厂方法构建bean");
+        SomeBean someBean = new SomeBean();
+        someBean.init();
+        System.out.println("create(): 工厂方法构建bean");
+        return someBean;
+    }
+
+    public SomeBean() {
+        System.out.println("SomeBean(): 构造方法");
+    }
+
+    public void init() {
+        System.out.println("init(): 初始化方法");
+    }
+
+    public void close() {
+        System.out.println("close(): 销毁方法");
+    }
+
+    public void sayHello() {
+        System.out.println("sayHello(): ");
+    }
+}
