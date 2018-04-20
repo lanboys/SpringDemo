cdec2a1cdc91c4c966845228ae70c2761ec76c9e
lanbing <lanbing@baogongyoucai.com>
2018-04-20 16:44:25 +0800

    重新调整分包

---
+++ src/main/java/com/bing/lan/spring/day1/ioc/_02_factory/SomeBean.java
@@ -0,0 +1,12 @@
+package com.bing.lan.spring.day1.ioc._02_factory;
+
+/**
+ * Created by 蓝兵 on 2018/4/19.
+ */
+
+class SomeBean {
+
+    public void sayHello() {
+        System.out.println("sayHello(): ");
+    }
+}
---
+++ src/main/java/com/bing/lan/spring/day1/ioc/_03_container/SomeBean.java
@@ -0,0 +1,16 @@
+package com.bing.lan.spring.day1.ioc._03_container;
+
+/**
+ * Created by 蓝兵 on 2018/4/19.
+ */
+
+public class SomeBean {
+
+    public SomeBean() {
+        System.out.println("SomeBean(): 构造方法");
+    }
+
+    public void sayHello() {
+        System.out.println("sayHello(): ");
+    }
+}
