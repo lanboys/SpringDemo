ae3c59752c0801ab60808a0cfe50be00165e7bd3
lanbing <lanbing@baogongyoucai.com>
2018-04-20 18:50:55 +0800

    bean属性的注入

---
+++ src/main/java/com/bing/lan/spring/day1/di/DiTest.java
@@ -0,0 +1,23 @@
+package com.bing.lan.spring.day1.di;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class DiTest {
+
+    @Autowired
+    private ApplicationContext mBeanFactory;
+
+    private SomeBean someBean;
+    @Test
+    public void test() {
+        someBean = mBeanFactory.getBean("someBean", SomeBean.class);
+        System.out.println("test(): " + someBean.toString());
+    }
+}
