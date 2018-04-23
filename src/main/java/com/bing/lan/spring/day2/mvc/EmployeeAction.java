98418e6efdba8ea3562552ad795f48f14ef83548
lanbing <lanbing@baogongyoucai.com>
2018-04-23 17:17:46 +0800

    用全注解修改mvc应用

--- src/main/java/com/bing/lan/spring/day2/mvc/EmployeeAction.java
+++ src/main/java/com/bing/lan/spring/day2/mvc/EmployeeAction.java
@@ -1,13 +1,18 @@
-package com.bing.lan.spring.day1.mvc;
+package com.bing.lan.spring.day2.mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by 蓝兵 on 2018/4/20.
  */
 
+@Controller
 public class EmployeeAction {
 
     private IEmployeeService employeeService;
 
+    @Autowired
     public void setEmployeeService(IEmployeeService employeeService) {
         this.employeeService = employeeService;
     }
